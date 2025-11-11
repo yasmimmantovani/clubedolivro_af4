901126ac8fc9c90fe8ddaf4e8375ba8aaf80c537
Yasmim <yasmimmantovani10@gmail.com>
2025-11-11 19:27:32 -0300

    Enviando tema claro/escuro, alterações no index etc

---
+++ js/theme.js
@@ -0,0 +1,20 @@
+const botaoTema = document.getElementById('tema');
+const  html = document.documentElement;
+
+if(localStorage.getItem('tema') === 'escuro') {
+    html.setAttribute('data-tema', 'escuro');
+    botaoTema.innerHTML = '<ion-icon name="sunny-outline"></ion-icon>';
+}
+
+botaoTema.addEventListener('click', () => {
+    const temaAtual = html.getAttribute('data-tema');
+
+    if (temaAtual === 'escuro') {
+        html.removeAttribute('data-tema');
+        botaoTema.innerHTML = '<ion-icon name="moon-outline"></ion-icon>';
+    } else {
+        html.setAttribute('data-tema', 'escuro');
+        botaoTema.innerHTML = '<ion-icon name="sunny-outline"></ion-icon>';
+        localStorage.setItem('tema', 'escuro');
+    }
+})
