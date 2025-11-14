ee7d749d53f4f5825b700a742e34f4dadd9b7e35
Yasmim <yasmimmantovani10@gmail.com>
2025-11-14 15:52:19 -0300

    Alterações no DB (Incompleto)

--- sql/banco_clube.sql
+++ sql/banco_clube.sql
@@ -1,12 +1,22 @@
 create database clubelivro;
 
-create table usuarios (
-    id_us int primary key auto_increment,
+create table usuarios_adm (
+    id int primary key auto_increment,
     nome varchar(100) not null,
     email varchar(100) unique not null,
     telefone varchar(20),
-    senha  varchar(255) not null
+    senha  varchar(255) not null,
+    nivel enum('admin', 'funcionario') default 'funcionario',
+    data_cadastro datetime now()
 );
+
+create table clientes (
+    id_clients int primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    senha varchar(255) not null,
+    
+)
 
 create table livros (
     id_livro int primary key auto_increment,
@@ -14,16 +24,17 @@ create table livros (
     autor varchar(100),
     ano int,
     genero varchar(50),
+    quantidade int not null default  1,
     disponibilidade enum('Disponível', 'Emprestado') default 'Disponível'
 );
 
 create table emprestimos (
     id_emprestimo int primary key auto_increment,
-    id_us int,
+    id_clients int,
     id_livro int,
     data_emprestimo date not null,
     data_devolucao date,
     status enum('Ativo', 'Devolvido') default 'Ativo',
-    foreign key (id_us) references usuarios(id_us) on delete cascade,
+    foreign key (id_clients) references clientes(id_clients) on delete cascade,
     foreign key (id_livro) references livros(id_livro) on delete cascade
 );
